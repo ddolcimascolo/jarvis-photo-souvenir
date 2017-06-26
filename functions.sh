5cc771aa53057f9cf531cf8fdd3057b1f1db3b44
ddolcimascolo <ddolcimascolo@linagora.com>
2017-06-26 09:27:34 +0200

    Now running chromium in the background. Make plugin ocmpatible with jarvis -x

--- functions.sh
+++ functions.sh
@@ -2,6 +2,8 @@
 
 function jv_pg_phs_takePhotoAndOpenIt() {
     jv_pg_phs_newFile
+    jv_pg_phs_closePhoto
+
     fswebcam -q -r ${jv_pg_phs_resolution} --no-banner -S ${jv_pg_phs_skipFrames} --jpeg ${jv_pg_phs_jpegQuality} $(jv_pg_phs_getLastPhoto)
 
     if [[ $? -eq 0 ]]; then
@@ -24,12 +26,16 @@ function jv_pg_phs_printPhoto() {
     say "$(jv_pg_phs_i18n PRINT_FAILED)"; return 1
 }
 
-function jv_pg_phs_closePhoto() {
-    killall chromium-browser
+function jv_pg_phs_showPhoto() {
+    chromium-browser --kiosk $(jv_pg_phs_getLastPhoto) > /dev/null 2>&1 & disown
+    echo $! > ${jv_pg_op_browserLockFile}
 }
 
-function jv_pg_phs_showPhoto() {
-    chromium-browser --kiosk $(jv_pg_phs_getLastPhoto) &
+function jv_pg_phs_closePhoto() {
+    if [[ -e ${jv_pg_op_browserLockFile} ]] && kill -0 $(cat ${jv_pg_op_browserLockFile}) > /dev/null 2>&1
+    then
+        kill $(cat ${jv_pg_op_browserLockFile})
+    fi
 }
 
 function jv_pg_phs_getLastPhoto() {
